2bdc3dd38ab2a9a465cdd2d5ae3769132451b92c
Mario Perez <perezmario303@gmail.com>
2025-01-25 20:30:00 +0100

    add gpu support

--- src/config.py
+++ src/config.py
@@ -101,3 +101,4 @@ VIZ_TRAIL_LENGTH = 500  # number of points to show in trail
 # GPU settings
 USE_GPU = True  # Try to use GPU if available
 GPU_BATCH_SIZE = 1024  # Process this many bodies at a time on GPU
+# gpu
